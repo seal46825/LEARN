3a07bfa70b3914f95866f2f1759af5e46b89d58f
seal46825 <54934688+seal46825@users.noreply.github.com>
2019-09-28 22:41:52 +0800

    Create Queue.c

---
+++ Data_Structure/Queue.c
@@ -0,0 +1,51 @@
+#define MAX 4
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int queue[MAX];
+int front=-1,rear=-1;
+
+int is_full(void)
+{
+    if(rear==MAX-1)
+        return 1;
+    return 0;
+}
+
+void enqueue (int a)
+{
+    if(is_full())
+        puts("已滿\n");
+    else
+        queue[++rear]=a;
+}
+
+void dequeue(void)
+{
+    if(front==rear)  //如果queue裡已經沒東西
+        puts("已空\n");
+    else
+        printf("元素%d已被取出\n",queue[++front]);
+
+}
+
+int main(void)
+{
+    int x;
+
+    while(1)
+    {
+        printf("輸入數字放進QUEUE 輸入-1取出:");
+        scanf("%d",&x);
+
+        if(x==-1)
+            dequeue();
+        else
+            enqueue(x);
+    }
+
+    return 0;
+}
+
+
