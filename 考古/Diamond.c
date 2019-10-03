1fed0dc3f92100576cbcdfb70b1b6893ea6e830e
seal46825 <54934688+seal46825@users.noreply.github.com>
2019-10-03 16:37:15 +0800

    Create Diamond.c

---
+++ 考古/Diamond.c
@@ -0,0 +1,83 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int i,j;
+int H,W=5;
+//star1~star4的撰寫都要用高度H去想比較好
+//因此要把寬度W轉換成高度H    H=(W+1)/2
+
+//star1、star2的高度H=寬度W
+//star3、star4的上半菱形高度為(W+1)/2
+//star4就是把star3的上半菱形補上下半部 記得i要從H-2開始
+
+void star1(void){
+    puts("Star1:");
+    H=W;
+    for(i=0;i<H;i++){
+        for(j=0;j<i+1;j++)
+            printf("*");
+        puts("");
+    }
+}
+
+void star2(void){   //因為H=5有5行 每一行有五個元素 星星加空白要等於5
+    puts("Star2:");
+    H=W;
+    for(i=0;i<H;i++){
+        for(j=0;j<H-i-1;j++)
+            printf(" ");
+        for(j=0;j<i+1;j++)
+            printf("*");
+        puts("");
+    }
+}
+
+
+void star3(void){
+    puts("Star3:");
+    int H=(W+1)/2;
+    for(i=0;i<H;i++){
+        for(j=0;j<H-i-1;j++)
+            printf(" ");
+        for(j=0;j<i*2+1;j++)
+            printf("*");
+        puts("");
+    }
+
+}
+
+void star4(void){
+    puts("Star4:");
+    int H=(W+1)/2;
+
+    for(i=0;i<H;i++){
+        for(j=0;j<H-i-1;j++)
+            printf(" ");
+        for(j=0;j<i*2+1;j++)
+            printf("*");
+        puts("");
+    }
+
+    //原本印到H-1 因為下半部要少一階 因此從H-2開始
+    for(i=H-2;i>=0;i--){
+        for(j=0;j<H-i-1;j++)
+            printf(" ");
+        for(j=0;j<i*2+1;j++)
+            printf("*");
+        puts("");
+    }
+
+}
+
+int main()
+{
+   int i;
+   star1();
+   star2();
+   star3();
+   star4();
+
+
+   return(0);
+}
+
