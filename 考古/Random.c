5276cba200665632965f99d6cb455d9e6bccc17f
seal46825 <54934688+seal46825@users.noreply.github.com>
2019-10-25 00:09:14 +0800

    Update Random.c

--- 考古/Random.c
+++ 考古/Random.c
@@ -1,4 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM 20   //數字範圍0 ~ NUM-1
+#define SIZE 20 //要印幾次數字
 
+int main(void)
+{
+    int i,a,cnt=0;
+    int num=NUM;
+    int *arr;
+    arr=(int*)malloc(num*sizeof(int));
+
+    for(i=0;i<num;i++)
+        arr[i]=i;
+
+    while(cnt<SIZE){
+
+        a=rand()%num;  //隨機取數
+        printf("%d ",arr[a]);
+        
+        arr[a]=arr[num-1];  //把印過的元素丟掉 最後一個元素補過來
+        num--;
+        
+        cnt++;
+    }
+}
+
+
+
+/* 舊版 最後會一直撞到 不好
 #include <stdio.h>
 #include <stdlib.h>
 #define NUM 501   //數字範圍0 ~ NUM-1
@@ -23,3 +52,4 @@ int main(void)
         cnt++;
     }
 }
+*/
